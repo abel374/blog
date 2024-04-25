28952c12383d5974d9bf3ba3806c4e04cbe3eb4d
Abel Cassinda <83987670+abel374@users.noreply.github.com>
2024-04-25 19:34:18 +0100

    Atualizado

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
 return (
-  <div>
-    <h1 className="text-center"> Sejam bem vindo ao blog de Notícias da Systan</h1>
+  <div className="text-center">
+    <h1 className="text-center"> Sejam todos bem vindo ao blog de Notícias da Systan</h1>
   </div>
 )
 }
