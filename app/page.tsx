b6bddf68ed3bff7b9aa4da71506e6f41e019b7a4
Abel Cassinda <83987670+abel374@users.noreply.github.com>
2024-04-25 19:33:04 +0100

    Atualizar a pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
 return (
   <div>
-    <h1>Sejam bem vindo ao blog de Notícias da Systan</h1>
+    <h1 className="text-center"> Sejam bem vindo ao blog de Notícias da Systan</h1>
   </div>
 )
 }
