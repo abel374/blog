42da9c3dc241829233d4dd069549bbc80367492d
abel <abelcassinda004@gmail.com>
2024-04-26 02:22:50 +0100

    atualizando

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
 return (
-  <div>
+  <div className="">
     <h1>Sejam bem vindo ao blog de Notícias da Systan</h1>
+    <h2>QUANDO DOR FALAR MAL DE MIM, APROVEITA E FALA TODAS AS COISAS BOAS QUE EU FIZ QUANDO VOCÊ PRECISOU</h2>
+    <h3 className=""> A VIDA É UMA ESCOLA, ONDE TODOS OS DIAS APRENDEMOS NOVAS COISAS. TENHA UM BOM TRABALHO.</h3>
+    <h1 className="flex text-center">GRAFICA AMAZONAS</h1>
   </div>
 )
 }
