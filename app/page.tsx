2dc65cdfaa6a594c40daa297648f7815963ef097
abel <abelcassinda004@gmail.com>
2024-04-26 20:48:26 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-
+import Image from "next/image";
+import home_bird from '@/public/img/home_bird.png'
 
 export default function Home() {
-return (
-  <div className="flex-auto center">
-    <h1>Sejam bem vindo ao blog de Notícias da Systan</h1>
-    <h2>QUANDO FOR FALAR MAL DE MIM, APROVEITA E FALA TODAS AS COISAS BOAS QUE EU FIZ QUANDO VOCÊ PRECISOU</h2>
-    <h3 className=""> A VIDA É UMA ESCOLA, ONDE TODOS OS DIAS APRENDEMOS NOVAS COISAS. TENHA UM BOM TRABALHO.</h3>
-    <h1 className="relative text-center">GRAFICA AMAZONAS</h1>
-  </div>
-)
+  return (
+    <div className="container flex flex-col md:flex-row gap-5 h-[calc(100vh-4rem)]">
+      <div className="basis-full flex flex-col justify-center md:basis-2/3">
+        <p className="special-word text-xs">Proteja todos os pássaros</p>
+        <h1 className="pb-5">
+          As aves <span className="special-word">Mais Rara</span><br /> do Mundo
+        </h1>
+
+        <p>Sejam Todos bem-vindos ao site de Notícias da Systan, fique ligado o que acontence em Angola e no Mundo.</p>
+      </div>
+
+      <div className="hidden md:block basis-1/3">
+        <Image
+          src={home_bird}
+          alt="Home bird"
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
 }
