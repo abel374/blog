6d6aa9f2d89579da49cb62275fd3f25721aef7ce
abel <abelcassinda004@gmail.com>
2024-04-26 12:24:59 +0100

    do jeito certo

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 export default function Home() {
 return (
-  <div className="">
+  <div className="flex-auto center">
     <h1>Sejam bem vindo ao blog de Notícias da Systan</h1>
     <h2>QUANDO FOR FALAR MAL DE MIM, APROVEITA E FALA TODAS AS COISAS BOAS QUE EU FIZ QUANDO VOCÊ PRECISOU</h2>
     <h3 className=""> A VIDA É UMA ESCOLA, ONDE TODOS OS DIAS APRENDEMOS NOVAS COISAS. TENHA UM BOM TRABALHO.</h3>
-    <h1 className="flex text-center">GRAFICA AMAZONAS</h1>
+    <h1 className="relative text-center">GRAFICA AMAZONAS</h1>
   </div>
 )
 }
